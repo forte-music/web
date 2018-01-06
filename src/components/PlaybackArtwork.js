0ecd63f7ba619a696619a7e4d4ca530b53dd404f
Martin Charles <martincharles07@gmail.com>
2018-01-05 23:08:33 -0500

    Added backgroundInteraction and More States to PlaybackArtwork
    Also made the size of the button smaller.

--- src/components/PlaybackArtwork.js
+++ src/components/PlaybackArtwork.js
@@ -11,9 +11,19 @@ import {
   overlay as overlayClass,
   path as pathClass,
   buttonBackdrop as buttonBackdropClass,
+  active as activeClass,
+  backgroundInteraction as backgroundInteractionClass,
 } from './PlaybackArtwork.css';
 
-type PlaybackState =
+export type PlaybackState =
+  // In this state, a play button is shown on hover. onStartPlayback is
+  // called when the button is pressed.
+  | 'STOPPED'
+
+  // In this state, a loading indicator is shown. No interaction can happen
+  // in this state.
+  | 'LOADING'
+
   // In this state a playing indicator is shown. onPaused is called when the
   // indicator is pressed.
   | 'PLAYING'
@@ -26,6 +36,10 @@ type Props = {
   // The child node will be rendered under the playing cover.
   children: Node,
 
+  // Whether or not to handle clicks on the background. This is false by
+  // default to allow parent components to handle click through.
+  backgroundInteraction?: boolean,
+
   // The current state of playback.
   state: PlaybackState,
 
@@ -34,16 +48,35 @@ type Props = {
 
   // Called when the playback state should change from PLAYING to PAUSED.
   onPaused: () => void,
+
+  // Called when the playback state should change from STOPPED to PLAYING.
+  onStartPlayback: () => void,
 };
 
 // An which renders an overlay of the current playing state atop an element
 // (usually Artwork or a Collage) and reacts to events.
-const PlaybackArtwork = ({ children, state, onPlaying, onPaused }: Props) => (
-  <div className={containerClass}>
+const PlaybackArtwork = ({
+  backgroundInteraction,
+  children,
+  state,
+  onPlaying,
+  onPaused,
+  onStartPlayback,
+}: Props) => (
+  <div
+    className={[
+      containerClass,
+      state !== 'STOPPED' ? activeClass : '',
+      backgroundInteraction ? backgroundInteractionClass : '',
+    ].join(' ')}
+  >
     <div className={childrenClass}>{children}</div>
 
     {/* Tints the children but still allows pointer events through. */}
-    <div className={overlayClass} />
+    <div
+      className={overlayClass}
+      onClick={() => handleUpdate(state, onPaused, onPlaying, onStartPlayback)}
+    />
 
     {/* The play/pause button in the bottom right corner. */}
     <div className={buttonBackdropClass}>
@@ -51,16 +84,27 @@ const PlaybackArtwork = ({ children, state, onPlaying, onPaused }: Props) => (
         svgClass={buttonClass}
         pathClass={pathClass}
         playing={state === 'PLAYING'}
-        onToggle={() => {
-          if (state === 'PLAYING') {
-            onPaused();
-          } else if (state === 'PAUSED') {
-            onPlaying();
-          }
-        }}
+        onToggle={() =>
+          handleUpdate(state, onPaused, onPlaying, onStartPlayback)
+        }
       />
     </div>
   </div>
 );
+
+const handleUpdate = (
+  state: PlaybackState,
+  onPaused: () => void,
+  onPlaying: () => void,
+  onStartPlayback: () => void
+) => {
+  if (state === 'PLAYING') {
+    onPaused();
+  } else if (state === 'PAUSED') {
+    onPlaying();
+  } else if (state === 'STOPPED') {
+    onStartPlayback();
+  }
+};
 
 export default PlaybackArtwork;
--- src/stories/PlaybackArtwork.js
+++ src/stories/PlaybackArtwork.js
@@ -1,27 +1,50 @@
+// @flow
 import React from 'react';
+import type { Node } from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 
 import { collage } from './Collage';
 import PlaybackArtwork from '../components/PlaybackArtwork';
 import StatefulComponent from './StatefulComponent';
+import type { PlaybackState } from '../components/PlaybackArtwork';
 
 storiesOf('PlaybackArtwork', module)
-  .add('playing', () => <Story initialState={'PLAYING'} />)
-  .add('paused', () => <Story initialState={'PAUSED'} />)
-  .add('with link', () => (
-    <Story initialState={'PAUSED'}>
+  .add('playing with background interaction', () => (
+    <Story initialState={'PLAYING'} />
+  ))
+  .add('paused with background interaction', () => (
+    <Story initialState={'PAUSED'} />
+  ))
+  .add('stopped with background interaction', () => (
+    <Story initialState={'STOPPED'} />
+  ))
+  .add('loading with background interaction', () => (
+    <Story initialState={'LOADING'} />
+  ))
+  .add('paused with link without background interaction', () => (
+    <Story initialState={'PAUSED'} backgroundInteraction={false}>
       <a onClick={action('link clicked')}>{collage}</a>
     </Story>
   ));
 
-const Story = ({ initialState, children = collage }) => (
+const Story = ({
+  initialState,
+  children = collage,
+  backgroundInteraction = true,
+}: {
+  initialState: PlaybackState,
+  children?: Node,
+  backgroundInteraction?: boolean,
+}) => (
   <StatefulComponent state={{ playback: initialState }}>
     {({ playback }, setState) => (
       <PlaybackArtwork
         state={playback}
         onPlaying={() => setState({ playback: 'PLAYING' })}
         onPaused={() => setState({ playback: 'PAUSED' })}
+        onStartPlayback={() => setState({ playback: 'PLAYING' })}
+        backgroundInteraction={backgroundInteraction}
       >
         {children}
       </PlaybackArtwork>
