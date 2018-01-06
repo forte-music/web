4dc4370472e1012cd90b98661d2de8014b619909
Martin Charles <martincharles07@gmail.com>
2018-01-06 16:43:05 -0500

    Added Interactive Playlist Story

--- src/stories/Playlist.js
+++ src/stories/Playlist.js
@@ -1,36 +1,40 @@
 // @flow
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
 
 import Playlist from '../containers/Playlist/Playlist';
 import SONGS from '../model';
 import type { PlaylistItem, Song, Edge } from '../model';
+import StatefulComponent from './StatefulComponent';
 
 storiesOf('Playlist', module).add('interactive', () => (
-  <Playlist
-    fetchMore={() => {}}
-    loading={false}
-    playlist={{
-      id: '1',
-      name: 'Break Up Music',
-      duration: 1 * 60 * 60 + 25 * 60 + 20,
-      items: {
-        count: 3,
-        edges: Object.values(SONGS).map((song: any, index): Edge<
-          PlaylistItem
-        > => ({
-          cursor: index.toString(),
-          node: {
-            id: index.toString(),
-            song: (song: Song),
+  <StatefulComponent state={{ playbackState: 'STOPPED' }}>
+    {({ playbackState }, setState) => (
+      <Playlist
+        fetchMore={() => {}}
+        loading={false}
+        playlist={{
+          id: '1',
+          name: 'Break Up Music',
+          duration: 1 * 60 * 60 + 25 * 60 + 20,
+          items: {
+            count: 3,
+            edges: Object.values(SONGS).map((song: any, index): Edge<
+              PlaylistItem
+            > => ({
+              cursor: index.toString(),
+              node: {
+                id: index.toString(),
+                song: (song: Song),
+              },
+            })),
           },
-        })),
-      },
-    }}
-    onPause={action('onPause')}
-    onPlay={action('onPlay')}
-    onStartPlayback={action('onStartPlayback')}
-    state={'STOPPED'}
-  />
+        }}
+        onPause={() => setState({ playbackState: 'PAUSED' })}
+        onPlay={() => setState({ playbackState: 'PLAYING' })}
+        onStartPlayback={() => setState({ playbackState: 'PLAYING' })}
+        state={playbackState}
+      />
+    )}
+  </StatefulComponent>
 ));
