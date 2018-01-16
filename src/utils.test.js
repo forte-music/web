d58d3fe417028154a21c08056fb2e8db4d95a96d
Martin Charles <martincharles07@gmail.com>
2018-01-15 20:11:07 -0500

    Utility Tests Describe Block

--- src/utils.test.js
+++ src/utils.test.js
@@ -1,17 +1,19 @@
 // @flow
 import { formatDuration, join, split } from './utils';
 
-it('should format a mm:ss duration correctly', () =>
-  expect(formatDuration(100)).toBe('01:40'));
+describe('formatDuration', () => {
+  it('should format a mm:ss duration correctly', () =>
+    expect(formatDuration(100)).toBe('01:40'));
 
-it('should format a 00:00 duration correctly', () =>
-  expect(formatDuration(0)).toBe('00:00'));
+  it('should format a 00:00 duration correctly', () =>
+    expect(formatDuration(0)).toBe('00:00'));
 
-it('should format a 00:ss duration correctly', () =>
-  expect(formatDuration(20)).toBe('00:20'));
+  it('should format a 00:ss duration correctly', () =>
+    expect(formatDuration(20)).toBe('00:20'));
 
-it('should format an hh:mm:ss duration correctly', () =>
-  expect(formatDuration(3601)).toBe('01:00:01'));
+  it('should format an hh:mm:ss duration correctly', () =>
+    expect(formatDuration(3601)).toBe('01:00:01'));
+});
 
 it('should join things', () => {
   const joined = join([1, 2, 3], Infinity);
