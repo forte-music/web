ad8521223acf3bc2901b65a4712ea262b77d61e7
Martin Charles <martincharles07@gmail.com>
2018-01-20 12:19:11 -0500

    Added Storyshot Smoke Tests

--- src/stories/storyshots.test.js
+++ src/stories/storyshots.test.js
@@ -1,7 +1,5 @@
-import initStoryshots, {
-  renderOnly,
-  imageSnapshot,
-} from '@storybook/addon-storyshots';
+import initStoryshots, { renderOnly } from '@storybook/addon-storyshots';
 
-initStoryshots({ suite: 'Smoke StoryShots', test: renderOnly });
-initStoryshots({ suite: 'Image StoryShots', test: imageSnapshot });
+const configPath = './config/storybook';
+
+initStoryshots({ suite: 'Smoke StoryShots', configPath, test: renderOnly });
