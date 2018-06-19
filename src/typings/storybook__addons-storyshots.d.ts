cb934148fd648440fd41e5974f8b2fda74c9aa5a
Martin Charles <martincharles07@gmail.com>
2018-06-18 23:54:50 -0400

    Added Typing Export
    Something keeps removing this. I'm not sure what.

--- src/typings/storybook__addons-storyshots.d.ts
+++ src/typings/storybook__addons-storyshots.d.ts
@@ -8,7 +8,7 @@ declare module '@storybook/addon-storyshots' {
   type TestFn = (options: { story: any; context: any; renderFrom: any }) => any;
 
   declare function initStoryshots(options?: Options): void;
-  declare const renderOnly: TestFn;
+  declare export const renderOnly: TestFn;
 
   export default initStoryshots;
 }
