f232ab1249fb94be6f4823386c8969b3b6d7546a
Martin Charles <martincharles07@gmail.com>
2018-06-19 21:45:57 -0400

    Set Default Jest Env to jsdom
    The configuration from ejecting set it to node.

--- jest.config.js
+++ jest.config.js
@@ -1,46 +1,43 @@
 const paths = require('./config/paths');
 
 module.exports = {
-    "collectCoverageFrom": [
-      "src/**/*.{ts,tsx}"
-    ],
-    "setupFiles": [
-      "<rootDir>/config/polyfills.js",
-      "<rootDir>/config/setupTests.js",
-    ],
-    "testMatch": [
-      "<rootDir>/src/**/__tests__/**/*.{ts,tsx}",
-      "<rootDir>/src/**/?(*.)(spec|test).{ts,tsx}"
-    ],
-    "testEnvironment": "node",
-    "testURL": "http://localhost",
-    "transform": {
-      "\\.js$": "babel-jest",
-      "\\.tsx?$": "ts-jest/preprocessor",
+  collectCoverageFrom: ['src/**/*.{ts,tsx}'],
+  setupFiles: [
+    '<rootDir>/config/polyfills.js',
+    '<rootDir>/config/setupTests.js',
+  ],
+  testMatch: [
+    '<rootDir>/src/**/__tests__/**/*.{ts,tsx}',
+    '<rootDir>/src/**/?(*.)(spec|test).{ts,tsx}',
+  ],
+  testURL: 'http://localhost',
+  transform: {
+    '\\.js$': 'babel-jest',
+    '\\.tsx?$': 'ts-jest/preprocessor',
+  },
+  transformIgnorePatterns: [
+    '[/\\\\]node_modules[/\\\\].+\\.(js|jsx|mjs|ts|tsx)$',
+  ],
+  moduleNameMapper: {
+    '^react-native$': 'react-native-web',
+    '\\.css$': 'identity-obj-proxy',
+  },
+  moduleFileExtensions: [
+    'web.ts',
+    'ts',
+    'web.tsx',
+    'tsx',
+    'web.js',
+    'mjs',
+    'js',
+    'json',
+    'web.jsx',
+    'jsx',
+    'node',
+  ],
+  globals: {
+    'ts-jest': {
+      tsConfigFile: paths.appTsTestConfig,
     },
-    "transformIgnorePatterns": [
-      '[/\\\\]node_modules[/\\\\].+\\.(js|jsx|mjs|ts|tsx)$',
-    ],
-    "moduleNameMapper": {
-      "^react-native$": "react-native-web",
-      "\\.css$": "identity-obj-proxy",
-    },
-    "moduleFileExtensions": [
-      "web.ts",
-      "ts",
-      "web.tsx",
-      "tsx",
-      "web.js",
-      "mjs",
-      "js",
-      "json",
-      "web.jsx",
-      "jsx",
-      "node"
-    ],
-    globals: {
-      'ts-jest': {
-        tsConfigFile: paths.appTsTestConfig,
-      },
-    },
+  },
 };
