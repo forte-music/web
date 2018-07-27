542eccb19de6974afec6dd4b50041d6c41c9664b
Martin Charles <martincharles07@gmail.com>
2018-07-26 23:35:05 -0400

    Added TInnerOwnProps and More Documentation

--- src/redux/render.tsx
+++ src/redux/render.tsx
@@ -5,19 +5,25 @@ import {
   MapStateToPropsParam,
 } from 'react-redux';
 
-export interface BaseOwnProps<TMergedProps> {
+export interface WithChildrenProp<TMergedProps> {
   children: (mergedProps: TMergedProps) => React.ReactElement<any> | null;
 }
 
 // Creates a component which passes redux enhanced props its children. It is
 // connected to redux with mapStateToProps and mapDispatchToProps.
 export function createReduxComponent<
+  // Redux State
   State,
+  // Props returned by mapStateToProps
   TStateProps extends {},
+  // Props returned by mapDispatchToProps
   TDispatchProps extends {},
-  TOwnProps extends BaseOwnProps<TStateProps & TDispatchProps> = BaseOwnProps<
-    TStateProps & TDispatchProps
-  >
+  // Helper type parameter to avoid having to pass in children prop type.
+  TInnerOwnProps extends {} = {},
+  // Props expected as input to the created component.
+  TOwnProps extends WithChildrenProp<TStateProps & TDispatchProps> &
+    TInnerOwnProps = WithChildrenProp<TStateProps & TDispatchProps> &
+    TInnerOwnProps
 >(
   mapStateToProps: MapStateToPropsParam<TStateProps, TOwnProps, State>,
   mapDispatchToProps: MapDispatchToPropsParam<TDispatchProps, TOwnProps>
@@ -26,7 +32,7 @@ export function createReduxComponent<
   type EnhancedProps = TStateProps & TDispatchProps;
 
   // Props passed to the inner component passed to redux connect.
-  type MergedProps = BaseOwnProps<EnhancedProps> & EnhancedProps;
+  type MergedProps = WithChildrenProp<EnhancedProps> & EnhancedProps;
 
   // Component which will be connected with react-redux.
   const InnerComponent: React.ComponentType<MergedProps> = (
