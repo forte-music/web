4483287f93f5ef5641e96bb045bf6dfa1b744479
Martin Charles <martincharles07@gmail.com>
2018-07-28 18:06:26 -0400

    Added Many CSS Variables to Theme

--- src/theme.ts
+++ src/theme.ts
@@ -9,6 +9,25 @@ export const theme = {
   sidebarBackgroundColor: colors.secondaryColor,
   footerBackgroundColor: lighten(0.1, colors.secondaryColor),
   contentBackgroundColor: '#1b1b1b',
+  headerBackgroundColor: '#222222',
+  headerPrimaryTextColor: '#ffffff',
+  headerSecondaryTextColor: '#bbbbbb',
+  headerTertiaryTextColor: '#aaaaaa',
+
+  sizeVeryTiny: '4px',
+  sizeTiny: '8px',
+  sizeSmall: '16px',
+  sizeBase: '24px',
+  sizeMedium: '36px',
+  sizeLarge: '48px',
+
+  fontSizeVeryTiny: '12px',
+  fontSizeTiny: '14px',
+  fontSizeMedium: '24px',
+  fontSizeLarge: '40px',
+
+  secondaryButtonSize: '40px',
+  artworkSize: '160px',
 };
 
 export type Theme = typeof theme;
