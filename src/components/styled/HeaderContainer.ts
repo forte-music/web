88774559c237b818aec91a90c18d532899b78233
Martin Charles <martincharles07@gmail.com>
2018-07-28 20:03:39 -0400

    Fixed HeaderContainer Path

--- src/components/styled/HeaderContainer.ts
+++ src/components/styled/HeaderContainer.ts
@@ -1,4 +1,4 @@
-import styled from 'src/styled-components';
+import styled from '../../styled-components';
 
 export const HeaderContainer = styled.div`
   background: ${props => props.theme.headerBackgroundColor};
