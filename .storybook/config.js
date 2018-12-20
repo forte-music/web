a20a280d43a82d0f125659947e32d980b17b9f20
Martin Charles <martincharles07@gmail.com>
2018-12-20 01:02:33 -0500

    Fixed Storybook
    It was broken because of require.context.

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import requireContext from 'require-context.macro';
 import { configure, addDecorator } from '@storybook/react';
 import { RootThemeProvider } from '../src/components/App/RootThemeProvider';
 import './styles.css';
@@ -8,48 +9,7 @@ const themeDecorator = story => (
 );
 addDecorator(themeDecorator);
 
-// This condition actually should detect if it's an Node environment
-// https://stackoverflow.com/questions/38332094/how-can-i-mock-webpacks-require-context-in-jest/42191018#42191018
-if (typeof require.context === 'undefined') {
-  const fs = require('fs');
-  const path = require('path');
-
-  require.context = (
-    base = '.',
-    scanSubDirectories = false,
-    regularExpression = /\.js$/
-  ) => {
-    const files = {};
-
-    function readDirectory(directory) {
-      fs.readdirSync(directory).forEach(file => {
-        const fullPath = path.resolve(directory, file);
-
-        if (fs.statSync(fullPath).isDirectory()) {
-          if (scanSubDirectories) readDirectory(fullPath);
-
-          return;
-        }
-
-        if (!regularExpression.test(fullPath)) return;
-
-        files[fullPath] = true;
-      });
-    }
-
-    readDirectory(path.resolve(__dirname, base));
-
-    function Module(file) {
-      return require(file);
-    }
-
-    Module.keys = () => Object.keys(files);
-
-    return Module;
-  };
-}
-
-const req = require.context('../src', true, /\.stories\.tsx$/);
+const req = requireContext('../src', true, /\.stories\.tsx$/);
 
 configure(() => {
   req.keys().forEach(filename => req(filename));
